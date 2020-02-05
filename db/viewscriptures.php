78990d9af058d6267a16d4f6ef1f08af53f4132e
loganwolverine8 <loganwolverineskousen@gmail.com>
2020-02-05 16:06:06 -0700

    adding scripture documents

--- db/viewscriptures.php
+++ db/viewscriptures.php
@@ -1,14 +1,29 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Scripture Resources</title>
 </head>
 <body>
 
+	<h1> Scripture Resources </h1>
+
+
 	<?PHP
 $statement = $db->query('SELECT book, chapter, verse, content, FROM scriptures');
-$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+	$book = $row['book'];
+	$chapter = $row['chapter'];
+	$verse = $row['verse'];
+	$content = $row['content'];
+
+	echo "<p><strong>$book $chapter:$verse</strong> - \"$content\"<p>";
+} 
+
 ?>
+
+
 </body>
 </html>
 
