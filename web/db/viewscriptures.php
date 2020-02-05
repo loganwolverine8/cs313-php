af682ba20fa3371525f4a36990b69fb0bd9c3675
loganwolverine8 <loganwolverineskousen@gmail.com>
2020-02-05 16:10:33 -0700

    Added db to web

---
+++ web/db/viewscriptures.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Scripture Resources</title>
+</head>
+<body>
+
+	<h1> Scripture Resources </h1>
+
+
+	<?PHP
+$statement = $db->query('SELECT book, chapter, verse, content, FROM scriptures');
+
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+	$book = $row['book'];
+	$chapter = $row['chapter'];
+	$verse = $row['verse'];
+	$content = $row['content'];
+
+	echo "<p><strong>$book $chapter:$verse</strong> - \"$content\"<p>";
+} 
+
+?>
+
+
+</body>
+</html>
+
