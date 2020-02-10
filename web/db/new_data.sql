e6a6801c49af27b4e25ea4398cea161598ad22b4
loganwolverine8 <loganwolverineskousen@gmail.com>
2020-02-09 20:10:30 -0700

    fixed names of docs

---
+++ web/db/new_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO user (username, password)
+  VALUES ('loganwolverine', 'abc123');
+
+INSERT INTO item (product)
+  VALUES ('Hot Wheel');
+
+INSERT INTO PRICE (cost)
+  VALUES (1);
+
+INSERT INTO seller (name)
+  VALUES ('Hannah');
---
+++ web/db/shopping_site.php
@@ -0,0 +1,40 @@
+<?php
+
+
+function get_db() {
+	$db = NULL;
+
+	try {
+		// default Heroku Postgres configuration URL
+		$dbUrl = getenv('DATABASE_URL');
+
+		if (!isset($dbUrl) || empty($dbUrl)) {
+			
+			$dbUrl = "postgres://ta_user:ta_pass@localhost:5432";
+
+			
+		}
+
+	
+		$dbopts = parse_url($dbUrl);
+
+		$dbHost = $dbopts["host"];
+		$dbPort = $dbopts["port"];
+		$dbUser = $dbopts["user"];
+		$dbPassword = $dbopts["pass"];
+		$dbName = ltrim($dbopts["path"],'/');
+
+	
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	}
+	catch (PDOException $ex) {
+	
+	
+		echo "Error connecting to DB. Details: $ex";
+		die();
+	}
+
+	return $db;
+}
