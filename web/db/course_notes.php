52d98fb6fc3ff7a25cdf49682660e941543017b1
loganwolverine8 <loganwolverineskousen@gmail.com>
2020-02-22 19:19:28 -0700

    added interpage

---
+++ web/db/course_notes.php
@@ -0,0 +1,22 @@
+<?php>
+
+if (!isset($_GET['course_id']))
+{
+	die("Error, course id not specified..");
+}
+$course_id = htmlspecialchars($_GET['course_id']);
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Course Notes</title>
+</head>
+<body>
+<h1>Course Notes for course id <?php echo $course_id?></h1>
+
+</body>
+</html>
