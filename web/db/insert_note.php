4f5dee23b488653193e961d7e1d0055356475db1
loganwolverine8 <loganwolverineskousen@gmail.com>
2020-02-22 21:25:33 -0700

    Added insert_note.php

--- web/db/insert_note.php
+++ web/db/insert_note.php
@@ -9,7 +9,7 @@ $db = get_db();
 
 $stmt = $db->prepare('INSERT INTO note(course_id, content) VALUES (:course_id, :content);');
 $stmt->bindValue(':course_id', $course_id, PDO::PARAM_INT);
-$stmt->bindValue(':content', $content, PDO::PARAM_INT);
+$stmt->bindValue(':content', $content, PDO::PARAM_STR);
 $stmt->execute();
 
 $new_page = "course_notes.php?course_id=$course_id";
