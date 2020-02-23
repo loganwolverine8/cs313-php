bf05d4a7729e72e8398e1376aeda5adeadebaef5
loganwolverine8 <loganwolverineskousen@gmail.com>
2020-02-22 19:29:07 -0700

    added interpage

--- web/db/course_notes.php
+++ web/db/course_notes.php
@@ -16,7 +16,7 @@ $course_id = htmlspecialchars($_GET['course_id']);
 	<title>Course Notes</title>
 </head>
 <body>
-<h1>Course Notes for course id <?php echo $course_id?></h1>
+<h1>Course Notes for course id <?php echo $course_id ?></h1>
 
 <p> this is a note!!!!!!!</p>
 <p> this is a note!!!!!!!</p>
