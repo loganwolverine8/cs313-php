ff7f68cb9a4f66f0ad645b781ff410c87f402bc9
loganwolverine8 <loganwolverineskousen@gmail.com>
2020-02-22 19:46:05 -0700

    added interpage

--- web/db/course_notes.php
+++ web/db/course_notes.php
@@ -6,6 +6,19 @@ if (!isset($_GET['course_id']))
 }
 $course_id = htmlspecialchars($_GET['course_id']);
 
+include('dbConnect.php');
+$db = get_db();
+
+
+$stmt = $db->prepare('SELECT c.code, c.name, n.content FROM note n JOIN course c ON n.course_id = c.id WHERE c.id=:id');
+$stmt->bindValue(':id', $course_id, PDO::PARAM_INT);
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+$stmt->execute();
+$note_rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+$course_code = $notes_rows[0]['code'];
+
 ?>
 
 
@@ -16,12 +29,14 @@ $course_id = htmlspecialchars($_GET['course_id']);
 	<title>Course Notes</title>
 </head>
 <body>
-<h1>Course Notes for course id <?php echo $course_id ?></h1>
+<h1>Course Notes for <?php echo $course_code; ?></h1>
+<?php
+foreach ($note_rows as $note_row)
+{
+	$content = $note_row['content'];
+	echo "<p>$content</p>";
+}
 
-<p> this is a note!!!!!!!</p>
-<p> this is a note!!!!!!!</p>
-<p> this is a note!!!!!!!</p>
-<p> this is a note!!!!!!!</p>
-
+?>
 </body>
 </html>
